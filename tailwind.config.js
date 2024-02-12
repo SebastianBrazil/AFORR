4e84c092915838f89516ded702cb6bd3f54402ab
SebastianBrazil <sbrevesanchez088@gmail.com>
2024-02-11 19:43:19 -0800

    notes

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,8 +12,24 @@ module.exports = {
   ],
 }
 
-// npm install react-bootstrap bootstrap
-// npm install -D tailwindcss
-// npx tailwindcss init
-// npm install flowbite flowbite-react
-// npm run start
+// Personal Notes:
+  // npm install react-bootstrap bootstrap
+  // npm install -D tailwindcss
+  // npx tailwindcss init
+  // npm install flowbite flowbite-react
+  // npm run start
+
+// inside tailwind.config: 
+  // /** @type {import('tailwindcss').Config} */
+  // module.exports = {
+  //   content: [
+  //     "./src/**/*.{js,jsx,ts,tsx}",
+  //     "node_modules/flowbite-react/lib/esm/**/*.js",
+  //   ],
+  //   theme: {
+  //     extend: {},
+  //   },
+  //   plugins: [
+  //     require('flowbite/plugin')
+  //   ],
+  // }
